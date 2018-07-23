5f2cc4765be29488c3d349b6c8b4397e821d4d7e
YauheniPo <ip.popovich@mail.ru>
2018-07-24 00:31:42 +0300

    Updated by lombok lib

--- src/test/java/demo/test/Models/Mail.java
+++ src/test/java/demo/test/Models/Mail.java
@@ -1,107 +1,20 @@
 package demo.test.models;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 /**
  * The type Mail.
  */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
 public class Mail {
     private final String subject;
     private final String text;
     private final String fromAddress;
     private final String toAddress;
-
-    /**
-     * Instantiates a new Mail.
-     *
-     * @param subject     the subject
-     * @param text        the text
-     * @param fromAddress the from address
-     * @param toAddress   the to address
-     */
-    public Mail(String subject, String text, String fromAddress, String toAddress) {
-        this.subject = subject;
-        this.text = text;
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-    }
-
-    /**
-     * Gets subject.
-     *
-     * @return the subject
-     */
-    public String getSubject() {
-        return subject;
-    }
-
-    /**
-     * Gets text.
-     *
-     * @return the text
-     */
-    public String getText() {
-        return text;
-    }
-
-    /**
-     * Gets from address.
-     *
-     * @return the from address
-     */
-    public String getFromAddress() {
-        return fromAddress;
-    }
-
-    /**
-     * Gets to address.
-     *
-     * @return the to address
-     */
-    public String getToAddress() {
-        return toAddress;
-    }
-
-    /**
-     * Equals boolean.
-     *
-     * @param o the o
-     *
-     * @return the boolean
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Mail mail = (Mail) o;
-        return Objects.equals(subject, mail.subject) &&
-                Objects.equals(text, mail.text) &&
-                Objects.equals(fromAddress, mail.fromAddress) &&
-                Objects.equals(toAddress, mail.toAddress);
-    }
-
-    /**
-     * Hashcode
-     *
-     * @return the hashCode
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(subject, text, fromAddress, toAddress);
-    }
-
-    /**
-     * To string string.
-     *
-     * @return the string
-     */
-    @Override
-    public String toString() {
-        return "Mail{" +
-                "subject='" + subject + '\'' +
-                ", text='" + text + '\'' +
-                ", fromAddress='" + fromAddress + '\'' +
-                ", toAddress='" + toAddress + '\'' +
-                '}';
-    }
 }
--- src/test/java/demo/test/Models/TestDataMails.java
+++ src/test/java/demo/test/Models/TestDataMails.java
@@ -1,59 +1,16 @@
 package demo.test.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * The type Test data mails.
+ */
+@Getter
+@AllArgsConstructor
 public class TestDataMails {
     private final String senderMailLogin;
     private final String senderMailPassword;
     private final String recipientMailLogin;
     private final String recipientMailPassword;
-
-    /**
-     * Instantiates a new Test data mails.
-     *
-     * @param senderMailLogin       the sender mail login
-     * @param senderMailPassword    the sender mail password
-     * @param recipientMailLogin    the recipient mail login
-     * @param recipientMailPassword the recipient mail password
-     */
-    public TestDataMails(String senderMailLogin, String senderMailPassword, String recipientMailLogin, String recipientMailPassword) {
-        this.senderMailLogin = senderMailLogin;
-        this.senderMailPassword = senderMailPassword;
-        this.recipientMailLogin = recipientMailLogin;
-        this.recipientMailPassword = recipientMailPassword;
-    }
-
-    /**
-     * Gets sender mail login.
-     *
-     * @return the sender mail login
-     */
-    public String getSenderMailLogin() {
-        return senderMailLogin;
-    }
-
-    /**
-     * Gets sender mail password.
-     *
-     * @return the sender mail password
-     */
-    public String getSenderMailPassword() {
-        return senderMailPassword;
-    }
-
-    /**
-     * Gets recipient mail login.
-     *
-     * @return the recipient mail login
-     */
-    public String getRecipientMailLogin() {
-        return recipientMailLogin;
-    }
-
-    /**
-     * Gets recipient mail password.
-     *
-     * @return the recipient mail password
-     */
-    public String getRecipientMailPassword() {
-        return recipientMailPassword;
-    }
 }
